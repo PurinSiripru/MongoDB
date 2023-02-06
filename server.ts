0f1bc1f094df42b38c0f514cf4f1e4ef3d1e7b4c
PurinSiripru <122721492+PurinSiripru@users.noreply.github.com>
2023-02-06 10:07:58 +0700

    Add files via upload

--- server.ts
+++ server.ts
@@ -62,6 +62,12 @@ app.post('/', (request: Request, response: Response) => {
      if (request.body.type === 'School') {
         path = 'school'
     }
+    if (request.body.isDone != '' && newTask === '' && request.body.delete === undefined) {
+        const update_id = request.body.isDone
+        Todo.findOneAndUpdate({_id:update_id},[{$set:{isDone:{$not:"$isDone"}}}],()=>{
+            response.redirect(path) 
+        })
+    }
      if (newTask !== '') {
          const task = new Todo({
             task:newTask,
